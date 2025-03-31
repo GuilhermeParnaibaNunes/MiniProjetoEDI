7385fd1a8efc0a71d2632b964ac7c553af175f19
GuiPnj <guipynj@gmail.com>
2025-03-31 09:42:10 -0300

    Criação de Aluno.c

---
+++ Aluno.c
@@ -0,0 +1,13 @@
+/*Setter para um aluno*/
+    // - Retorna 1 para procedimento bem-sucedido;
+    // - Retorna 0 para procedimento malsucedido;
+    // - Retorna -1 para RGM inválido;
+    // - Recebe ponteiro para o objeto aluno.
+    // - Recebe RGM a ser cadastrado.
+int setAluno(t_Aluno *a, char *RGM){
+  if(sizeof(RGM)!=(8*sizeof(char)))
+    return -1;
+  a->RGM = RGM;
+    return 0;
+}
+
